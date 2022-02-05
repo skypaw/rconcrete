8e15498735005bd5de04e437fed033f251705867
skypaw <53307746+skypaw@users.noreply.github.com>
2022-02-05 22:01:10 +0100

    Test sonarcloud functionality

--- src/main.py
+++ src/main.py
@@ -2,3 +2,25 @@
 
 def sample_function(add: int) -> int:
     return add + add
+
+def function_for_sonarcloud_test():
+    pass
+
+def function_for_sonarcloud_test1():
+    pass
+def function_for_sonarcloud_test2():
+    pass
+def function_for_sonarcloud_test3():
+    pass
+def function_for_sonarcloud_test4():
+    pass
+def function_for_sonarcloud_test5():
+    pass
+def function_for_sonarcloud_test6():
+    pass
+def function_for_sonarcloud_test7():
+    pass
+def function_for_sonarcloud_test8():
+    pass
+def function_for_sonarcloud_test9():
+    pass
